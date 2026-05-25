56fcdee16989bec11ba984cb9afee084156e54ce
emilioxd14 <2510809671@stud.fh-kufstein.ac.at>
2026-05-25 15:51:15 +0200

    fix: Rebuild Router root in App.jsx to explicitly declare protected routes and add a catch-all redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import HubView from './views/HubView';
 import DetailView from './views/DetailView';
 import LoginView from './views/LoginView';
@@ -11,15 +11,13 @@ import ProtectedRoute from './components/ProtectedRoute';
 function App() {
   return (
     <Routes>
-      {/* Public */}
       <Route path="/login" element={<LoginView />} />
-
-      {/* Protected */}
       <Route path="/" element={<ProtectedRoute><HubView /></ProtectedRoute>} />
       <Route path="/plant/:id" element={<ProtectedRoute><DetailView /></ProtectedRoute>} />
       <Route path="/store" element={<ProtectedRoute><StoreView /></ProtectedRoute>} />
       <Route path="/billing" element={<ProtectedRoute><BillingView /></ProtectedRoute>} />
       <Route path="/shipping" element={<ProtectedRoute><ShippingView /></ProtectedRoute>} />
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
 }
